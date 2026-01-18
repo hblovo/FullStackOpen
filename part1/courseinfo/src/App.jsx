efd3eee67d43b1cfdb19ba433c13e195c8a878c2
hblovo <694186378@qq.com>
2026-01-18 17:28:58 +0800

    1.1 - 1.5

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,58 @@
+
+const Header = (props) =>{
+    return <h1>{props.name}</h1>
+}
+const Part = (props) => {
+    return (
+        <div>
+            <h2>
+                {props.parts.name}
+            </h2>
+            <p>Exercises number is <b>{props.parts.exercises}</b></p>
+        </div>
+    )
+}
+const Content = (props) => {
+    return (
+        <div>
+            <Part parts={props.parts[0]} />
+            <Part parts={props.parts[1]} />
+            <Part parts={props.parts[2]} />
+        </div>
+    )
+}
+const Total = (props)=>{
+    let total = 0;
+    props.parts.forEach(part =>{
+        total += part.exercises
+    })
+    return <h2>Total number of exercises is <b>{total}</b></h2>
+}
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <div>
+             <Header name={course.name} />
+             <Content parts={course.parts} />
+             <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default App

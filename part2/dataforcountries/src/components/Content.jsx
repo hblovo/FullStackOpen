87595b5cd3ce4c827356fb92d07cbab284e8c3be
hblovo <694186378@qq.com>
2026-01-20 07:46:03 +0800

    2.20

--- part2/dataforcountries/src/components/Content.jsx
+++ part2/dataforcountries/src/components/Content.jsx
@@ -1,11 +1,40 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+const WeatherDetail = ({capital,weather})=>{
+    if (!weather) return null
+    return(
+        <div>
+            <h2>Weather in {capital}</h2>
+            <p>Temperature is {(weather.main.temp-273.15).toFixed(2)} Celsius</p>
+            <img
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            />
+            <p>Wind {(weather.wind.speed).toFixed(1)} m/s</p>
+        </div>
+    )
+}
 export const CountryDetail = ({ country }) => {
+    const [weather,setWeather] = useState()
+    const capital = country.capital[0]
+    const API_KEY = `316f4701e497c8e5a9a4c1a81e525275`
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${API_KEY}`)
+            .then(response => {
+                setWeather(response.data)
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.log('Error:', error)
+            })
+    }, [capital])
     return (
         <div>
             <h1>{country.name.common}</h1>
             <div>capital {country.capital[0]}</div>
             <div>area {country.area}</div>
 
-            <h3>Languages:</h3>
+            <h2>Languages:</h2>
             <ul>
                 {Object.values(country.languages).map(lang => (
                     <li key={lang}>{lang}</li>
@@ -16,6 +45,7 @@ export const CountryDetail = ({ country }) => {
                 alt={`Flag of ${country.name.common}`}
                 width="150"
             />
+            <WeatherDetail capital={capital} weather={weather} />
         </div>
     )
 }

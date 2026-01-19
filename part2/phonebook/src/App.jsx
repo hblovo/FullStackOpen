954cdb313cc84a73015ede04bd51e327c48eaf2f
hblovo <694186378@qq.com>
2026-01-19 17:29:47 +0800

    2.12-2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React, {useEffect, useState} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newPhone,setNewPhone] = useState('')
     const [ filter,setFilter] = useState('')
     const personsToShow = filter === '' ? persons
         :persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+    const fetchPersons = ()=>{
+        personService.getAll().then(
+            response=>{
+                console.log(response)
+                setPersons(response.data)
+            }
+        )
+    }
+    useEffect(fetchPersons, []);
 
     const handleFilterChange = (event)=>{
         setFilter(event.target.value)

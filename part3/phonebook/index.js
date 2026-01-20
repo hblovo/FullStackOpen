26133e82092af0d997079f0ea5e2c803c645ea80
hblovo <694186378@qq.com>
2026-01-20 18:49:27 +0800

    3.1 - 3.2

---
+++ part3/phonebook/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/',(request, response) => {
+    response.send('<h1>Hello PhoneBook Backend!</h1>')
+})
+app.get('/api/persons',(request, response) => {
+    response.json(persons)
+})
+app.get('/info',(request, response) => {
+    const date = new Date()
+    const count = persons.length
+    console.log(count)
+    const content = `
+        <p>Phonebook has info for ${count} people</p>
+        <p>${date}</p>
+    `
+    response.send(content)
+})
+const PORT = 3001
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})

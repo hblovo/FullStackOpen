3c937934f431ec5d30d9b7d28f73957006059f41
hblovo <694186378@qq.com>
2026-01-22 07:46:15 +0800

    4.17 - 4.18

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,8 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require("./controllers/users");
+const loginRouter = require('./controllers/login')
+
 
 mongoose.set('strictQuery', false)
 
@@ -26,5 +28,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users',usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,28 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+blogsRouter.get('/',async (request, response)=>{
+    const blogs = await Blog
+        .find({})
+        .populate('user','username name')
 
-blogsRouter.get('/',(request, response)=>{
-    Blog.find({}).then(blogs=>{
-        response.json(blogs)
-    })
+    response.json(blogs)
+
 })
 blogsRouter.post('/',async (request, response, next) => {
     const body = request.body
+    const user = await User.findOne({})
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes || 0,
+        user: user._id
     })
     try {
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
         response.status(200).json(savedBlog)
     } catch (e) {
         next(e)
